8fa37befeb7c3dd37461a1afc031db29d4d4432d
Runedp1 <rune.de.peet@gmail.com>
2024-10-21 15:05:05 +0200

    added env

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-const express = require('express')
+const express = require('express');
+const Database = require('./classes/database.js');
+
+// Aanmaken van een express app
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.send('Hello world!');
+// Endpoints
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.get('/api/artists', (req,res)=>{
-    res.send([
-        "JB",
-        "Beyonce",
-    ]);
+app.get('/api/artists', (req, res) => {
+    const db = new Database();
+    db.getQuery('SELECT * FROM artists').then((artists) => {
+        res.send(artists);
+    });
 });
 
-app.listen(3000, () =>{
-    console.log('Server is running on port 3000')
-})
+app.get('/api/songs', (req, res) => {
+    const db = new Database();
+    db.getQuery(`
+        SELECT
+            song_id, s.name AS songname, a.name AS artistname
+        FROM
+            songs AS s
+                INNER JOIN
+                    artists AS a
+                        ON
+                            s.artist_id = a.artist_id;
+    `).then((songs) => {
+        res.send(songs);
+    });
+});
+
+
+// Starten van de server en op welke port de server moet luistere
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
